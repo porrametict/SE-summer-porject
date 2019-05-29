3d66bb5824d39b2ea32e754e52ff3c03699982bf
porrametict <porrametict@gmail.com>
2019-05-29 14:35:16 +0700

    start-vue again

--- summer/routes/web.php
+++ summer/routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('ddprovinces','ProvinceDropdownController@index')->name('ddprovinces');
