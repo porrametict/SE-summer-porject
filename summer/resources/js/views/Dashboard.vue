2c6d6649119fd8399d1ff4c8bf7d1cd6e9febe7d
Nitikorn <Nitikorn>
2019-05-30 15:47:33 +0700

    commit

--- summer/resources/js/views/Dashboard.vue
+++ summer/resources/js/views/Dashboard.vue
@@ -2,19 +2,67 @@
     <div>
         <h1>Dashboard</h1>
         <hr>
-        <button class="btn btn-primary btn-lg"
-                v-on:click="btnAlert">
-            GOTO PAGE 2
-        </button>
+<!--        <button class="btn btn-primary btn-lg" v-on:click="btnAlert()">GOTO PAGE 2</button>-->
+<!--        <button class="btn btn-primary btn-lg" v-on:click="loadsex()">new</button>-->
+        <hr>
+        กรอกชื่อ<input v-model="name">
+        <hr>
+
+        <select v-model="sex">
+            <option :value="sex.id" v-for="sex in sexes">{{sex.text}}</option>
+        </select><br/>
+
+        <button class="btn btn-primary btn-lg" @click="cat()">Add</button><br/>
+        <h5>Users</h5>
+        <ul v-if="listuser.length > 0">
+            <li v-for="user in listuser">{{user.name}} - {{user.sex}}
+                <button class="btn btn-danger" v-on:click="getuser()">Add</button>
+            </li>
+        </ul>
+        <h1 v-else>
+            No Data
+        </h1>
     </div>
 </template>
+
+
 <script>
     export default {
-        methods : {
+        data: () => ({
+            sex: "",
+            sexes: null,
+            name: "",
+            listuser: [
+                {name: "dog", sex: "ชาย"},
+                {name: "cat", sex: "หญิง"}
+            ]
+        }),
+        created() {
+            this.loadsex()
+        },
+        methods: {
             btnAlert() {
-                this.$router.push({name:"page2"})
+                this.$router.push({name: "page2"})
+            },
+            async loadsex() {
+                let data = await axios.get('/api/sexes',)
+                    .then(function (response) {
+                        console.log(response.data);
+                        return response.data;
+                    })
+                    .catch(function (error) {
+                        console.log(error.data);
+                        return null;
+                    })
+                this.sexes = data;
+            },
+            getuser() {
+                let name = this.name;
+                let sex = this.sexes[this.sex-1].text;
+                console.log(name,sex)
+                this.listuser.push({"name":name,"sex":sex})
             }
-
         }
     }
+
 </script>
