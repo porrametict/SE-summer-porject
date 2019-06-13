6a25aef37921b0343d7e72f464a9d9613d66a680
Pond <pungpondstr@gmail.com>
2019-06-13 16:12:40 +0700

    commit Create Component

--- summer/app/Survey.php
+++ summer/app/Survey.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
   protected $fillable = ['name', 'u_id', 'status'];
+
+  public function questions(){
+      return $this -> hasMany('App\Questions', 's_id', 'id');
+  }
+
 }
