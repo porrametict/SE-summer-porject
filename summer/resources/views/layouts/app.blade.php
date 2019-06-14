ec8e8ff1b52e52d9855924fc6e345d1663088dd5
porrametict <porrametict@gmail.com>
2019-06-14 11:05:46 +0700

    change link icon

--- summer/resources/views/layouts/app.blade.php
+++ summer/resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #4dc0b5;">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/home#/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     BigSurvey
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
