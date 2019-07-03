770ab2b00430547976b8d42ed48361f199266da2
porrametict <porrametict@gmail.com>
2019-07-03 13:35:51 +0700

    fix comment model

--- summer/app/Comment.php
+++ summer/app/Comment.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['text', 's_id','age','sex','career'];
+    protected $fillable = ['text', 's_id','age','sex','career','province'];
 
 }
